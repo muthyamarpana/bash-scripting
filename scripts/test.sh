4095f37ae3dc85ce4a6e6813c1ef0a6b89a9b145
ArpanaM <muriki.arpana@gmail.com>
2025-05-03 06:44:09 +0530

    my first file

--- scripts/test.sh
+++ scripts/test.sh
@@ -9,3 +9,10 @@ echo -e "\e[33mHello in Yellow\e[0m"
 echo -e "\e[34mHello in Blue\e[0m"
 echo -e "\e[35mHello in Magenta\e[0m"
 echo -e "\e[36mHello in Cyan\e[0m"
+
+a=100
+echo a=$a
+echo a in currency = ${a}USD
+
+DATE=2025-05-03
+echo good morning, welcome to todays date -${DATE}
