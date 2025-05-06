fee1101ab1e491326181b37369ece7ad9111a471
ArpanaM <muriki.arpana@gmail.com>
2025-05-06 06:37:09 +0530

    conditions

--- scripts/conds.sh
+++ scripts/conds.sh
@@ -8,8 +8,10 @@ echo $age
 
 if [ -z  "${age}" -a "${age}" -lt 18 ]; then
   echo you are minor - $age
+  exit
   elif [ -z "${age}" -a "${age}" -gt 60 ]; then
     echo you are a senior citizen - $age
+    exit
   else
     echo you are major - $age
   fi
