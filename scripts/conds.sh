2fceb2ebff0ad0bf7180255c7e277e22875a7325
ArpanaM <muriki.arpana@gmail.com>
2025-05-06 06:36:12 +0530

    conditions

--- scripts/conds.sh
+++ scripts/conds.sh
@@ -1,14 +1,14 @@
 /#!bin/bash
 read -p "enter your age" age
 echo $age
-if [ -z "${age}" ]; then
-  echo input is missing
-  exit
-  fi
+#if [ -z "${age}" ]; then
+#  echo input is missing
+#  exit
+#  fi
 
-if [ "${age}" -lt 18 ]; then
+if [ -z  "${age}" -a "${age}" -lt 18 ]; then
   echo you are minor - $age
-  elif [ "${age}" -gt 60 ]; then
+  elif [ -z "${age}" -a "${age}" -gt 60 ]; then
     echo you are a senior citizen - $age
   else
     echo you are major - $age
