b4c623a963c34176d060ce84e7cfe5ddb992f0fb
ArpanaM <muriki.arpana@gmail.com>
2025-05-06 06:39:20 +0530

    conditions

--- scripts/conds.sh
+++ scripts/conds.sh
@@ -12,6 +12,9 @@ if [ -z  "${age}" -a "${age}" -lt 18 ]; then
   elif [ -z "${age}" -a "${age}" -gt 60 ]; then
     echo you are a senior citizen - $age
     exit
-  else
+    elif [ -z "${age}" ]; then
+      echo input is missing
+      exit
+    else
     echo you are major - $age
   fi
