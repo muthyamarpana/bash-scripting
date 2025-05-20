5c04017c1cf02511756a83766bc3c315460ca8ec
ArpanaM <muriki.arpana@gmail.com>
2025-05-20 05:57:05 +0530

    check httpcode of given url

--- scripts/printhttpcode.sh
+++ scripts/printhttpcode.sh
@@ -1,6 +1,6 @@
 #!/bin/bin
 read -p "enter the website to check the http code: " url
-http_code=$(curl --write-out %{http_code} --silent --output /dev/null)
+http_code=$(curl --write-out %{http_code} --silent --output /dev/null $url)
 echo $http_code
 
 
