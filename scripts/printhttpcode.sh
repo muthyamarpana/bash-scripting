2d3c93dc2929e8a29e21c4100d02fccb810aafc6
ArpanaM <muriki.arpana@gmail.com>
2025-05-20 06:00:49 +0530

    check httpcode of given url

--- scripts/printhttpcode.sh
+++ scripts/printhttpcode.sh
@@ -2,5 +2,10 @@
 read -p "enter the website to check the http code: " url
 http_code=$(curl --write-out %{http_code} --silent --output /dev/null $url)
 echo $http_code
+if [ $http_code -eq 200 ]; then
+  echo "The HTTP connection is success.."
+  else
+    echo "The HTTP connection is failure... "
+    fi
 
 
